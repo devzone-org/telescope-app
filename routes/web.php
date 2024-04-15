1bb727075226cd62941854d4dba4ebb7dfcf5b92
Ali <ali2020ahtisham@outlook.com>
2024-04-15 17:48:16 +0500

    updatE

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::any('{any}', function () {
+    // Handle any route here
+})->where('any', '.*');
 Route::get('/', function () {
     return view('welcome');
 });
